ec3aa33ec468f40f58cdbab7bf0925cbca907668
Vinci Darwin <WKC_Futurist@163.com>
2023-09-22 17:24:41 +0800

    Create Server,Client

---
+++ Server/Controllers/MarkdownLoaderController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WoBok_Website.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarkdownLoaderController : ControllerBase
+    {
+        [HttpGet("{fileName}")]
+        public string Get(string fileName)
+        {
+            string? markdownContent;
+            try
+            {
+                markdownContent = System.IO.File.ReadAllText($"D:/WoBokMarkdown/{fileName}.md");
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                return "Not Found!";
+            }
+            if (markdownContent == null)
+                return "empty!";
+            return markdownContent;
+        }
+    }
+}
