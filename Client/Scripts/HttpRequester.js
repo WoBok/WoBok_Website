9a454d6df05c14bc51fa2bc64aa210739597c99e
WoBok <darwin.vinci.wkc@gmail.com>
2023-10-03 20:13:46 +0800

    moudls

---
+++ Client/Scripts/HttpRequester.js
@@ -0,0 +1,31 @@
+async function fetchRequest(url, type) {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            switch (type) {
+                case 'text':
+                    return response.text();
+                case 'json':
+                    return response.json();
+                default:
+                    console.error(`Invalid type: ${type}`);
+            }
+        } else {
+            console.error(`Fetch ${type} failed: ${response.statusText}`);
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const httpRequest = {
+
+    fetchText: async function (url) {
+        return await fetchRequest(url, 'text');
+    },
+
+    fecthJson: async function (url) {
+        return await fetchRequest(url, 'json');
+    }
+};
---
+++ Client/Scripts/ModelImporter.js
@@ -0,0 +1,5 @@
+var marked = document.createElement('script');
+marked.src = 'https://cdn.jsdelivr.net/npm/marked/marked.min.js';
+document.head.appendChild(marked);
+
+import { httpRequest } from './HttpRequester.js'
